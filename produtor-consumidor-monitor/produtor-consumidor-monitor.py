86125bfbc3808cc07465d47cf0aab32922d1bede
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2025-04-15 18:31:55 -0300

    Alterando Produtor-consumidor monitor-2

--- produtor-consumidor-monitor/produtor-consumidor-monitor.py
+++ produtor-consumidor-monitor/produtor-consumidor-monitor.py
@@ -2,7 +2,7 @@ import threading
 import time
 import random
 
-class BufferMonitor:
+class bounded_buffer:
     def __init__(self, tamanho):
         self.buffer = []
         self.tamanho = tamanho
@@ -10,7 +10,7 @@ class BufferMonitor:
         self.not_empty = threading.Condition(self.lock)
         self.not_full = threading.Condition(self.lock)
 
-    def inserir(self, item, produtor_id):
+    def put_resource(self, item, produtor_id):
         with self.not_full:
             while len(self.buffer) == self.tamanho:
                 print(f"Produtor {produtor_id} esperando: buffer cheio\n")
@@ -21,7 +21,7 @@ class BufferMonitor:
             print(f'Buffer: {self.buffer}\n')
             self.not_empty.notify()  
 
-    def remover(self, consumidor_id):
+    def get_resource(self, consumidor_id):
         with self.not_empty:
             while len(self.buffer) == 0:
                 print(f"Consumidor {consumidor_id} esperando: buffer vazio\n")
@@ -35,28 +35,28 @@ class BufferMonitor:
             return item
 
 
-buffer_monitor = BufferMonitor(5)
+buffer_monitor = bounded_buffer(5)
 
 
 def produtor(id):
     while True:
-        item = f"item_{id}_{random.randint(0, 100)}"
+        item = f"item-{random.randint(0, 100)}"
         time.sleep(random.uniform(0.5, 1))
-        buffer_monitor.inserir(item, id)
+        buffer_monitor.put_resource(item, id)
 
 
 def consumidor(id):
     while True:
-        time.sleep(random.uniform(0.5, 2))
-        buffer_monitor.remover(id)
+        time.sleep(random.uniform(1, 2))
+        buffer_monitor.get_resource(id)
 
 
-for i in range(1):  
+for i in range(5):  
     t = threading.Thread(target=produtor, args=(i,))
     t.daemon = True
     t.start()
 
-for i in range(5):  
+for i in range(1):  
     t = threading.Thread(target=consumidor, args=(i,))
     t.daemon = True
     t.start()
@@ -66,4 +66,4 @@ try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    print("\nEncerrando o programa...")
+    print("\nFIM")
